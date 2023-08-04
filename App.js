ea19b9af683692e7b830710e8285b2adca45e3a4
Ahmad Piracha <ahmadpiracha3@gmail.com>
2023-08-05 03:14:28 +0500

    updated things

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ import SignUpScreen from "./screens/SignUpScreen";
 import MatchTeamCard from "./screens/MatchTeamCard";
 import ViewResultsScreen from "./screens/ViewResultsScreen";
 import MatchDetails from "./screens/MatchDetails";
-import { useNavigation } from "@react-navigation/native";
 import EquipmentBooking from "./screens/EquipmentBookingScreen";
 import SportsVenueBooking from "./screens/SportsVenueBooking";
 import ItemDetailsScreen from "./screens/ItemDetailsScreen";
@@ -26,118 +25,151 @@ const Stack = createNativeStackNavigator();
 
 // Custom Drawer Navigator
 const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator
-      drawerContent={(props) => <CustomDrawer {...props} />}
-      screenOptions={{
-        drawerLabelStyle: { marginLeft: -25, fontSize: 15 },
-        drawerActiveBackgroundColor: "#03045E",
-        drawerActiveTintColor: "#fff",
-        drawerInactiveTintColor: "#000",
-        headerShown: false,
-      }}
-    >
-      <Drawer.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name={focused ? "home" : "home-outline"}
-              size={size}
-              color={focused ? "#00B4D8" : "#000"}
-            />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="Sports Schedule"
-        component={SportsSchedule}
-        options={{
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name={focused ? "person" : "person-outline"}
-              size={size}
-              color={focused ? "#00B4D8" : "#000"}
-            />
-          ),
-        }}
-      />
+        return ( <
+            Drawer.Navigator drawerContent = {
+                (props) => < CustomDrawer {...props }
+                />}
+                screenOptions = {
+                    {
+                        drawerLabelStyle: { marginLeft: -25, fontSize: 15 },
+                        drawerActiveBackgroundColor: "#03045E",
+                        drawerActiveTintColor: "#fff",
+                        drawerInactiveTintColor: "#000",
+                        headerShown: false,
+                    }
+                } >
+                <
+                Drawer.Screen
+                name = "Home"
+                component = { HomeScreen }
+                options = {
+                    {
+                        drawerIcon: ({ focused, size }) => ( <
+                            Ionicons name = { focused ? "home" : "home-outline" }
+                            size = { size }
+                            color = { focused ? "#00B4D8" : "#000" }
+                            />
+                        ),
+                    }
+                }
+                /> <
+                Drawer.Screen
+                name = "Sports Schedule"
+                component = { SportsSchedule }
+                options = {
+                    {
+                        drawerIcon: ({ focused, size }) => ( <
+                            Ionicons name = { focused ? "person" : "person-outline" }
+                            size = { size }
+                            color = { focused ? "#00B4D8" : "#000" }
+                            />
+                        ),
+                    }
+                }
+                />
 
-      <Drawer.Screen
-        name="Booking"
-        component={SportsInventoryScreen}
-        options={{
-          drawerIcon: ({ focused, size }) => (
-            <Ionicons
-              name={focused ? "person" : "person-outline"}
-              size={size}
-              color={focused ? "#00B4D8" : "#000"}
-            />
-          ),
-        }}
-      />
-    </Drawer.Navigator>
-  );
-};
+                <
+                Drawer.Screen
+                name = "Booking"
+                component = { SportsInventoryScreen }
+                options = {
+                    {
+                        drawerIcon: ({ focused, size }) => ( <
+                            Ionicons name = { focused ? "person" : "person-outline" }
+                            size = { size }
+                            color = { focused ? "#00B4D8" : "#000" }
+                            />
+                        ),
+                    }
+                }
+                /> < /
+                Drawer.Navigator >
+            );
+        };
 
-const App = () => {
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+        const App = () => {
+            const [user, setUser] = useState(null);
+            const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    // Check if the user is logged in
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        setUser(authUser);
-      } else {
-        setUser(null);
-      }
-      setIsLoading(false);
-    });
+            useEffect(() => {
+                // Check if the user is logged in
+                const unsubscribe = auth.onAuthStateChanged((authUser) => {
+                    if (authUser) {
+                        setUser(authUser);
+                    } else {
+                        setUser(null);
+                    }
+                    setIsLoading(false);
+                });
 
-    // Clean up the listener on unmount
-    return () => unsubscribe();
-  }, []);
+                // Clean up the listener on unmount
+                return () => unsubscribe();
+            }, []);
 
-  if (isLoading) {
-    return null; // You can return a loading component here if needed
-  }
+            if (isLoading) {
+                return null; // You can return a loading component here if needed
+            }
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {user ? (
-          <>
-            <Stack.Screen name="DrawerRoot" component={DrawerNavigator} />
-            <Stack.Screen name="MatchTeamCard" component={MatchTeamCard} />
-            <Stack.Screen name="MatchDetails" component={MatchDetails} />
-            <Stack.Screen name="ViewResults" component={ViewResultsScreen} />
-            <Stack.Screen name="EquipmentBooking" component={EquipmentBooking} />
-            <Stack.Screen name="SportsVenueBooking" component={SportsVenueBooking} />
-            <Stack.Screen
-          name="ItemDetails"
-          component={ItemDetailsScreen}
-          options={{ title: "Item Details" }}
-        />
+            return ( <
+                NavigationContainer >
+                <
+                Stack.Navigator screenOptions = {
+                    { headerShown: false }
+                } > {
+                    user ? ( <
+                        >
+                        <
+                        Stack.Screen name = "DrawerRoot"
+                        component = { DrawerNavigator }
+                        /> <
+                        Stack.Screen name = "MatchTeamCard"
+                        component = { MatchTeamCard }
+                        /> <
+                        Stack.Screen name = "MatchDetails"
+                        component = { MatchDetails }
+                        /> <
+                        Stack.Screen name = "ViewResults"
+                        component = { ViewResultsScreen }
+                        /> <
+                        Stack.Screen name = "EquipmentBooking"
+                        component = { EquipmentBooking }
+                        /> <
+                        Stack.Screen name = "SportsVenueBooking"
+                        component = { SportsVenueBooking }
+                        /> <
+                        Stack.Screen name = "ItemDetails"
+                        component = { ItemDetailsScreen }
+                        options = {
+                            { title: "Item Details" }
+                        }
+                        />
 
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={SignUpScreen} />
-          </>
-        )}
-      </Stack.Navigator>
+                        <
+                        />
+                    ) : ( <
+                        >
+                        <
+                        Stack.Screen name = "Login"
+                        component = { LoginScreen }
+                        /> <
+                        Stack.Screen name = "Register"
+                        component = { SignUpScreen }
+                        /> < /
+                        >
+                    )
+                } <
+                /Stack.Navigator>
 
-      {/* <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="SportsInventoryScreen"
-          component={SportsInventoryScreen}
-        />
-      </Stack.Navigator> */}
-    </NavigationContainer>
-  );
-};
+                {
+                    /* <Stack.Navigator screenOptions={{ headerShown: false }}>
+                            <Stack.Screen
+                              name="SportsInventoryScreen"
+                              component={SportsInventoryScreen}
+                            />
+                          </Stack.Navigator> */
+                } <
+                /NavigationContainer>
+            );
+        };
 
-export default App;
+        export default App;
--- model/matchesData.js
+++ model/matchesData.js
@@ -1,177 +1,183 @@
-export const sliderData = [
-  {
-    title: "First Game",
-    image: require("../assets/images/homescreen/SliderImg1.jpg"),
-  },
-  {
-    title: "Second Game",
-    image: require("../assets/images/homescreen/SliderImg2.jpg"),
-  },
-  {
-    title: "Third Game",
-    image: require("../assets/images/homescreen/SliderImg3.jpg"),
-  },
+export const sliderData = [{
+        title: "First Game",
+        image: require("../assets/images/homescreen/SliderImg1.jpg"),
+    },
+    {
+        title: "Second Game",
+        image: require("../assets/images/homescreen/SliderImg2.jpg"),
+    },
+    {
+        title: "Third Game",
+        image: require("../assets/images/homescreen/SliderImg3.jpg"),
+    },
 ];
 
-export const TodaysMatch = [
-  {
+export const TodaysMatch = [{
     id: 1,
     homeTeam: "Manchester United",
     awayTeam: "Liverpool",
     venue: "Old Trafford",
     time: "19:45",
     date: "29th April 2023",
-  },
-];
+}, ];
 
-export const UpcomingMatches = [
-  {
-    id: 1,
-    homeTeam: "Bayern Munich",
-    awayTeam: "Borussia Dortmund",
-    venue: "Allianz Arena",
-    time: "18:30",
-    date: "7th May 2023",
-  },
+export const UpcomingMatches = [{
+        id: 1,
+        homeTeam: "Bayern Munich",
+        awayTeam: "Borussia Dortmund",
+        venue: "Allianz Arena",
+        time: "18:30",
+        date: "7th May 2023",
+    },
 
 ];
 
-export const SportsType = [
-  {
-    id: "1",
+export const SportsType = [{
+        id: "1",
+        title: "Cricket",
+        logo: "tennisball-sharp",
+        quantity: 0, // Initial count for Cricket
+        maxQuantity: 5, // Maximum count for Cricket
 
-    title: "Cricket",
-    logo: "tennisball-sharp",
-  },
-  {
-    id: "2",
-    title: "Futsal",
-    logo: "football-sharp",
-  },
-  {
-    id: "3",
-    title: "Basketball",
-    logo: "american-football-sharp",
-  },
-  {
-    id: "4",
-    title: "Batminton",
-    logo: "basketball-sharp",
-  },
+    },
+    {
+        id: "2",
+        title: "Futsal",
+        logo: "football-sharp",
+        quantity: 0, // Initial count for Futsal
+        maxQuantity: 10, // Maximum count for Cricket
+
+    },
+    {
+        id: "3",
+        title: "Basketball",
+        logo: "american-football-sharp",
+        quantity: 0, // Initial count for Basketball
+        maxQuantity: 5, // Maximum count for Cricket
+
+    },
+    {
+        id: "4",
+        title: "Badminton",
+        logo: "basketball-sharp",
+        quantity: 0, // Initial count for Badminton
+        maxQuantity: 0, // Maximum count for Cricket
+
+    },
 ];
 
-export const TeamAPlayers = [
-  {
-    id: 1,
-    name: "John Smith",
-    number: 10,
-  },
-  {
-    id: 2,
-    name: "Emily Johnson",
-    number: 7,
-  },
-  {
-    id: 3,
-    name: "Michael Williams",
-    number: 3,
-  },
-  {
-    id: 4,
-    name: "Jessica Brown",
-    number: 5,
-  },
-  {
-    id: 5,
-    name: "Daniel Davis",
-    number: 9,
-  },
-  {
-    id: 6,
-    name: "Olivia Rodriguez",
-    number: 11,
-  },
-  {
-    id: 7,
-    name: "Matthew Martinez",
-    number: 2,
-  },
-  {
-    id: 8,
-    name: "Sophia Taylor",
-    number: 8,
-  },
-  {
-    id: 9,
-    name: "Andrew Anderson",
-    number: 6,
-  },
-  {
-    id: 10,
-    name: "Emma Thomas",
-    number: 13,
-  },
-  {
-    id: 11,
-    name: "David Clark",
-    number: 4,
-  },
+
+
+export const TeamAPlayers = [{
+        id: 1,
+        name: "John Smith",
+        number: 10,
+    },
+    {
+        id: 2,
+        name: "Emily Johnson",
+        number: 7,
+    },
+    {
+        id: 3,
+        name: "Michael Williams",
+        number: 3,
+    },
+    {
+        id: 4,
+        name: "Jessica Brown",
+        number: 5,
+    },
+    {
+        id: 5,
+        name: "Daniel Davis",
+        number: 9,
+    },
+    {
+        id: 6,
+        name: "Olivia Rodriguez",
+        number: 11,
+    },
+    {
+        id: 7,
+        name: "Matthew Martinez",
+        number: 2,
+    },
+    {
+        id: 8,
+        name: "Sophia Taylor",
+        number: 8,
+    },
+    {
+        id: 9,
+        name: "Andrew Anderson",
+        number: 6,
+    },
+    {
+        id: 10,
+        name: "Emma Thomas",
+        number: 13,
+    },
+    {
+        id: 11,
+        name: "David Clark",
+        number: 4,
+    },
 ];
 
-export const TeamBPlayers = [
-  {
-    id: 1,
-    name: "James Johnson",
-    number: 20,
-  },
-  {
-    id: 2,
-    name: "Isabella Thompson",
-    number: 14,
-  },
-  {
-    id: 3,
-    name: "William Lee",
-    number: 19,
-  },
-  {
-    id: 4,
-    name: "Mia Walker",
-    number: 15,
-  },
-  {
-    id: 5,
-    name: "Ethan Hall",
-    number: 17,
-  },
-  {
-    id: 6,
-    name: "Ava Lewis",
-    number: 23,
-  },
-  {
-    id: 7,
-    name: "Alexander Young",
-    number: 22,
-  },
-  {
-    id: 8,
-    name: "Charlotte King",
-    number: 18,
-  },
-  {
-    id: 9,
-    name: "Benjamin Hill",
-    number: 21,
-  },
-  {
-    id: 10,
-    name: "Harper Wright",
-    number: 16,
-  },
-  {
-    id: 11,
-    name: "Christopher Turner",
-    number: 12,
-  },
+export const TeamBPlayers = [{
+        id: 1,
+        name: "James Johnson",
+        number: 20,
+    },
+    {
+        id: 2,
+        name: "Isabella Thompson",
+        number: 14,
+    },
+    {
+        id: 3,
+        name: "William Lee",
+        number: 19,
+    },
+    {
+        id: 4,
+        name: "Mia Walker",
+        number: 15,
+    },
+    {
+        id: 5,
+        name: "Ethan Hall",
+        number: 17,
+    },
+    {
+        id: 6,
+        name: "Ava Lewis",
+        number: 23,
+    },
+    {
+        id: 7,
+        name: "Alexander Young",
+        number: 22,
+    },
+    {
+        id: 8,
+        name: "Charlotte King",
+        number: 18,
+    },
+    {
+        id: 9,
+        name: "Benjamin Hill",
+        number: 21,
+    },
+    {
+        id: 10,
+        name: "Harper Wright",
+        number: 16,
+    },
+    {
+        id: 11,
+        name: "Christopher Turner",
+        number: 12,
+    },
 ];
