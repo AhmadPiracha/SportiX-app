e711690a5b1e1492900c1e2ac64cd4dceefceec9
Ahmad Piracha <ahmadpiracha3@gmail.com>
2023-08-14 02:52:42 +0500

    fetched data from DB for players name and roll number

--- components/PlayerList.jsx
+++ components/PlayerList.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, SafeAreaView } from "react-native";
 import { windowWidth } from "../utils/dimensions";
+import axios from "axios";
 
-const PlayersList = ({ name,rollNumber }) => {
+const PlayersList = ({ team }) => {
+  const [playerData, setPlayerData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://192.168.10.9:5001/getPlayers?team=${team}`, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response?.data) {
+          setPlayerData(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <View style={styles.playerContainer}>
-          <View style={{ width: windowWidth - 220,flexDirection: 'row', alignItems: 'center',justifyContent:'space-evenly' }}>
-            <Text style={styles.playerName}>{name}</Text>
-            <Text style={styles.playerName}>{rollNumber}</Text>
-
+        {playerData.map((item) => (
+          <View key={item.id} style={styles.playerContainer}>
+            <View style={styles.playerMain}>
+              <Text style={styles.playerName}>{item.playername}</Text>
+              <Text style={styles.rollNumber}>{item.rollno}</Text>
+            </View>
+            <View style={styles.separator} />
           </View>
-        </View>
+        ))}
       </View>
     </SafeAreaView>
   );
@@ -20,21 +44,35 @@ const PlayersList = ({ name,rollNumber }) => {
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: 8,
+    flex: 1,
     paddingHorizontal: 16,
+    paddingTop: 8,
   },
   playerContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-    paddingBottom: 8,
+    backgroundColor: "#0d1b2a",
+    paddingVertical: 12,
+  },
+  playerMain: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   playerName: {
     fontSize: 16,
     fontWeight: "bold",
-    marginLeft: 8,
-    color: "#ffffff",
+  },
+  rollNumber: {
+    fontSize: 16,
+    color: "#ffffff", 
+  },
+  playerName: {
+    fontSize: 16,
+    color: "#ffffff", 
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "#ffffff",
+    marginTop: 8,
   },
 });
 
