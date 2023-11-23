1b7ea7ae95e3959c48b846106a093c7f546ac0d7
Ahmad Piracha <ahmadpiracha3@gmail.com>
2023-11-23 11:50:33 +0500

    final work started

---
+++ screens/Bidding/BiddingMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const BiddingMenu = () => {
+  return (
+    <View>
+        <Text>
+            Bidding Menu
+        </Text>
+    </View>
+  )
+}
+
+export default BiddingMenu
---
+++ screens/Bidding/FPLBidding.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+
+const FPLBidding = () => {
+    return (
+        <View>
+            <Text>
+                FPL Bidding
+            </Text>
+        </View>
+
+    )
+}
+
+export default FPLBidding
