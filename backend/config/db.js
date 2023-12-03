2cecaaaba8f72fe90a6bc10de5222a99d22e4692
Ahmad Piracha <ahmadpiracha3@gmail.com>
2023-12-03 22:50:38 +0500

    bidding API created

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const os = require('os');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json({ type: "application/json" }));
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
@@ -22,16 +22,16 @@ const connection = mysql.createConnection({
 });
 module.exports = connection;
 
-app.get("/getTeams", function(req, res) {
+app.get("/getTeams", function (req, res) {
     let sql = "SELECT * FROM sportix.teams WHERE type='cricket'";
 
-    connection.query(sql, function(err, results) {
+    connection.query(sql, function (err, results) {
         if (err) throw err;
         res.send(results);
     });
 });
 
-app.get("/teamSchedule", function(req, res) {
+app.get("/teamSchedule", function (req, res) {
     const type = req.query.type || null;
     let sql = "SELECT * FROM sportix.teamschedule";
 
@@ -39,7 +39,7 @@ app.get("/teamSchedule", function(req, res) {
         sql += " WHERE type = ?";
     }
 
-    connection.query(sql, [type], function(err, results) {
+    connection.query(sql, [type], function (err, results) {
         if (err) {
             console.error("Error executing SQL query:", err);
             return res.status(500).send("Error fetching team schedules");
@@ -49,10 +49,10 @@ app.get("/teamSchedule", function(req, res) {
     });
 });
 
-app.get("/getPlayers", function(req, res) {
+app.get("/getPlayers", function (req, res) {
     const team = req.query.team;
     connection.query("SELECT playername,rollno FROM sportix.player WHERE team = ?", [team],
-        function(err, results) {
+        function (err, results) {
             if (err) {
                 console.error("Error executing SQL query:", err);
                 return res.status(500).send("Error fetching players");
@@ -247,6 +247,87 @@ app.get('/getLeagueSchedule', (req, res) => {
         res.json(result);
     });
 });
+
+// app.post('/placeBid', async (req, res) => {
+//     try {
+//         const { displayName, userRollNo, team, league, basePrice, biddingAmount } = req.body;
+//         const totalAmount = basePrice + biddingAmount;
+
+//         const result =
+//             'INSERT INTO league_bidding (displayName, userRollNo, team, league, basePrice, biddingAmount, totalAmount, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())';
+//         const values =
+//             [displayName, userRollNo, team, league, basePrice, biddingAmount, totalAmount];
+
+//         connection.query(result, values, (err) => {
+//             if (err) {
+//                 console.error("Error executing SQL query:", err);
+//                 return res.status(500).json({ message: "Error inserting data" });
+//             }
+//             res.status(200).json({ message: "Bid placed successfully" });
+//         }
+//         );
+//     }
+//     catch (err) {
+//         console.error("Error executing SQL query:", err);
+//         return res.status(500).json({ message: "Error inserting data" });
+//     }
+// }
+// );
+
+
+app.post('/placeBid', async (req, res) => {
+    try {
+        const { displayName, userRollNo, team, league, basePrice, biddingAmount } = req.body;
+        const totalAmount = basePrice + biddingAmount;
+
+        // Check if there are existing bids for the same team and league
+        const existingBidsQuery = 'SELECT * FROM league_bidding WHERE team = ? AND league = ?';
+        const existingBidsValues = [team, league];
+
+        connection.query(existingBidsQuery, existingBidsValues, (err, result) => {
+            if (err) {
+                console.error("Error executing SQL query:", err);
+                return res.status(500).json({ message: "Error checking existing bids" });
+            }
+
+            if (result.length === 0 || totalAmount > result[0].totalAmount) {
+                // The current bid is the highest or there are no existing bids, update or insert accordingly
+                let query;
+                let values;
+
+                if (result.length === 0) {
+                    // No existing bids, insert a new record
+                    query = 'INSERT INTO league_bidding (displayName, userRollNo, team, league, basePrice, biddingAmount, totalAmount, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())';
+                    values = [displayName, userRollNo, team, league, basePrice, biddingAmount, totalAmount];
+                } else {
+                    // Existing bids, update the highest bid
+                    query = 'UPDATE league_bidding SET displayName = ?, userRollNo = ?, basePrice = ?, biddingAmount = ?, totalAmount = ?, createdAt = NOW() WHERE team = ? AND league = ?';
+                    values = [displayName, userRollNo, basePrice, biddingAmount, totalAmount, team, league];
+                }
+
+                connection.query(query, values, (err) => {
+                    if (err) {
+                        console.error("Error executing SQL query:", err);
+                        return res.status(500).json({ message: "Error updating or inserting bid" });
+                    }
+
+                    res.status(200).json({ success: true, message: 'Bid placed successfully' });
+                });
+            } else {
+                // The current bid is not the highest
+                res.status(200).json({ success: false, message: 'Your bid is not the highest' });
+            }
+        });
+    } catch (err) {
+        console.error("Error placing bid:", err);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
+
+
+
+
 
 
 app.listen(5001, () => {
